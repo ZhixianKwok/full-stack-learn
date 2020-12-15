250c1c72b6534fee879347c1917f6ac93f634ec7
guozhixian <zhixiankwok@gmail.com>
2020-12-15 22:57:15 +0800

    finish part1 - exercise 1.14
    anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,27 +1,31 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 const App = (props) => {
-  const [selected,setSelected] = useState(0)
-  const [votes,setVotes] = useState([])
-  
-
   const {anecdotes} = props
+  const [selected,setSelected] = useState(0)
+  const [votes,setVotes] = useState(new Array(anecdotes.length).fill(0))
+  
   const handleOnClick = () => {
     const randomInt = parseInt(Math.floor(Math.random() * anecdotes.length),10)
     setSelected(randomInt)
   }
   const handleOnVote = () => {
     const votesCopy = [...votes]
-    votesCopy[selected] = votesCopy[selected] ? votesCopy[selected]+1:1
+    votesCopy[selected] = votesCopy[selected] + 1
     setVotes(votesCopy)
   }
+
+  const max = Math.max.apply(null,votes)
+  const index = votes.findIndex(item=>item === max)
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <p>has {votes[selected]??0} votes</p>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
       <button onClick={handleOnVote}>vote</button>
       <button onClick={handleOnClick}>next anecdote</button>
+      <h3>Anecdotes with most votes</h3>
+      <p>{max > 0 && anecdotes[index]}</p>
     </div>
   )
 }
