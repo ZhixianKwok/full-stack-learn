f017439f29309b8347ed89e8783020fa86602939
guozhixian <zhixiankwok@gmail.com>
2020-12-15 16:01:24 +0800

    finish part1 - exercise 1.3
    course information step3

--- part1/course-info/src/components/Content.js
+++ part1/course-info/src/components/Content.js
@@ -3,8 +3,8 @@ import Part from './Part'
 
 export default (props) => {
     return <fragment>
-        <Part part={props.part1} exercise={props.exercise1}/>
-        <Part part={props.part2} exercise={props.exercise2}/>
-        <Part part={props.part3} exercise={props.exercise3}/>
+        <Part part={props.part1.name} exercise={props.part1.exercise}/>
+        <Part part={props.part2.name} exercise={props.part2.exercise}/>
+        <Part part={props.part3.name} exercise={props.part3.exercise}/>
     </fragment>
 }
--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -6,18 +6,26 @@ import Total from './components/Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercise1 = 10
-  const part2 = 'Using props to pass data'
-  const exercise2 = 7
-  const part3 = 'State of a component'
-  const exercise3 = 14
+  const part1 = {
+    name:'Fundamentals of React',
+    exercise:10
+  }
+
+  const part2 = {
+    name:'Using props to pass data',
+    exercise:7
+  }
+
+  const part3 = {
+    name:'State of a component',
+    exercise:14
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercise1={exercise1} exercise2={exercise2} exercise3={exercise3}/>
-      <Total total={exercise1 + exercise2 + exercise3}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total total={part1.exercise1 + part2.exercise2 + part3.exercise3}/>
     </div>
   )
 }
