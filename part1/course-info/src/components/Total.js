59ac58f8f7f809b4a00e92ccae9e5f7853330028
guozhixian <zhixiankwok@gmail.com>
2020-12-15 16:14:14 +0800

    finish part1 - exercise 1.5
    course information step5

--- part1/course-info/src/components/Total.js
+++ part1/course-info/src/components/Total.js
@@ -2,6 +2,6 @@ import react from 'react'
 
 export default (props) => {
     return <p>
-        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+        Number of exercises {props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}
     </p>
 }
