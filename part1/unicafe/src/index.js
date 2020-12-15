3eacc8e90b3b1cffcad16e407637557396ace53f
guozhixian <zhixiankwok@gmail.com>
2020-12-15 17:33:58 +0800

    finish part1 - exercise 1.9
    unicafe step4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Statistics = (props) => {
@@ -9,29 +9,29 @@ const Statistics = (props) => {
 }
 
 const App = () => {
-   const [good,setGood] = useState(0)
-   const [neutral,setNeutral] = useState(0)
-   const [bad,setBad] = useState(0)
-   const sum = good + neutral + bad,
-         average = (good * 1 + neutral * 0 + bad * -1) / sum,
-         positive = good / sum
-   
-   return (
-     <div>
-       <h2>give feedback</h2>
-       <button onClick={()=>setGood(good+1)}>good</button>
-       <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-       <button onClick={()=>setBad(bad+1)}>bad</button>
-       <h2>statistics</h2>
-       <p>good {good}</p>
-       <p>neutral {neutral}</p>
-       <p>bad {bad}</p>
-       {!!sum && <Statistics average={average} positive={positive}/>}
-     </div> 
-   )
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const sum = good + neutral + bad,
+    average = (good * 1 + neutral * 0 + bad * -1) / sum,
+    positive = good / sum
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>statistics</h2>
+      {sum ? <fragment><p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <Statistics average={average} positive={positive} /></fragment> : <p>No feedback given</p>}
+    </div>
+  )
 }
 
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
 );
