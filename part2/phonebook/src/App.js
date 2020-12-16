9a001d7654b51de706b35648fcdd969d8cdbdbcd
guozhixian <zhixiankwok@gmail.com>
2020-12-16 16:09:16 +0800

    finish part2 - exercise 2.10
    The Phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then(res=>{
+      console.log('promise fulfilled')
+      setPersons(res.data)
+    })
+  },[])
 
   const [ newName , setNewName ] = useState('')
   const [ newNumber , setNewNumber ] = useState('')
@@ -51,7 +56,6 @@ function App() {
       <PersonForm handleOnSubmit={handleOnSubmit} handleOnChangeName = { handleOnChangeName } handleOnChangeNumber ={ handleOnChangeNumber }/>
       <h2>Numbers</h2>
       <Persons filterPersons = {filterPersons} />
-
     </div>
   );
 }
