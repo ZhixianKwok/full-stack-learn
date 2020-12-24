99757cdc2e498b1ed64b184fc949efe07d5d0fa3
guozhixian <zhixiankwok@gmail.com>
2020-12-24 14:47:25 +0800

    part4 - exercise 4.5
    helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) =>{
   return blogs.reduce((prev, cur) => prev + cur.likes,0)
 }
 
+const favoriteLikes = (blogs) =>{
+  const sortBlogs = blogs.sort((a,b)=> b.likes - a.likes)
+  if(sortBlogs.length){
+    return sortBlogs[0]
+  }
+  return null
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteLikes
 }
