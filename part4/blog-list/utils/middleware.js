ea6b7de1af384d961406c105c64e4298ee3a5ba1
guozhixian <zhixiankwok@gmail.com>
2020-12-30 18:20:53 +0800

    part4 - exercise 4.15 - 4.22
    - finish part4 - exercise 4.15 - 4.22
    - change README.md

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,6 +1,6 @@
 const logger = require('./logger')
 
-const requestLogger = (req , res ,next) => {
+const requestLogger = (req , _ ,next) => {
   logger.info('Method:',req.method)
   logger.info('Path:',req.path)
   logger.info('Body:',req.body)
@@ -8,22 +8,33 @@ const requestLogger = (req , res ,next) => {
   next()
 }
 
-const unknownEndpoint = (req,res) => {
+const unknownEndpoint = ( _ ,res) => {
   res.status(404).send({error:'unknown endpoint'})
 }
 
-const errorHandler = (error, req, res, next) => {
-  logger.error(error.message)
+const errorHandler = (error, _ , res, next) => {
   if(error.name === 'CastError' && error.kind === 'ObjectId'){
     return res.status(400).send({error:'malformatted id'})
   } else if(error.name === 'ValidationError'){
     return res.status(400).json({error:error.message})
+  } else if( error.name === 'JsonWebTokenError'){
+    return res.status(401).json({error:error.message})
   }
   next(error)
+}
+
+const tokenExtractor = (req , _ , next) => {
+  const authorization = req.get('authorization')
+  req.token = null
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')){
+    req.token = authorization.substring(7)
+  }
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
