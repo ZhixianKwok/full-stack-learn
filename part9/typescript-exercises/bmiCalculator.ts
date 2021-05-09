5d83915277aa1a3bb2965670598404facb886dea
guozhixian <zhixiankwok@gmail.com>
2021-05-09 17:41:52 +0800

    part8 - exercise 9.1 - 9.3
    - Body mass index
    - Exercise calculator
    - Command line

--- part9/typescript-exercises/bmiCalculator.ts
+++ part9/typescript-exercises/bmiCalculator.ts
@@ -1,20 +1,21 @@
+function calculateBmi(height: number, weight: number): string {
+  let bmi: number = 0;
 
-function calculateBmi(height:number,weight:number):string{
-    let bmi:number = 0
-
-    if(height<=0 || weight<=0){
-        throw new Error('height and weight must be greater than zero')
-    }
-    bmi = weight / ((height / 100) ** 2)
-    //BMI正常值在20至25之间，超过25为超重，30以上则属肥胖。
-    switch(true){
-        case bmi < 20:
-            return `Slim (${height} ${weight})`
-        case bmi >= 20 && bmi <=25:
-            return `Normal (${height} ${weight})`
-        default:
-            return `Fat (${height} ${weight})`
-    }
+  if (height <= 0 || weight <= 0) {
+    throw new Error("height and weight must be greater than zero");
+  }
+  bmi = weight / (height / 100) ** 2;
+  //BMI正常值在20至25之间，超过25为超重，30以上则属肥胖。
+  switch (true) {
+    case bmi < 20:
+      return `Slim (${height} ${weight})`;
+    case bmi >= 20 && bmi <= 25:
+      return `Normal (${height} ${weight})`;
+    default:
+      return `Fat (${height} ${weight})`;
+  }
 }
 
-console.log(calculateBmi(180, 74))
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
+console.log(calculateBmi(height, weight));
