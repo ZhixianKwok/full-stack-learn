e8730cf3ea877792828b864277cc13d9ddf5c0f1
guozhixian <zhixiankwok@gmail.com>
2021-05-09 17:41:52 +0800

    part9 - exercise 9.1 - 9.3
    - Body mass index
    - Exercise calculator
    - Command line

---
+++ part9/typescript-exercises/calculateExercises.ts
@@ -0,0 +1,48 @@
+interface TrainingEffeact {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number,
+}
+
+function calculateExercises(traningHoursOfDay: number[], target: number):TrainingEffeact {
+    
+    const periodLength = traningHoursOfDay.length,
+          trainingDays = traningHoursOfDay.filter(item=>item > 0).length,
+          sumTrainingHours = traningHoursOfDay.reduce((prev,next)=> prev + next, 0),
+          average = sumTrainingHours / periodLength;
+
+    let rating = 0,
+        ratingDescription = "bad",
+        success = false;
+          
+    
+    if( average > target ) {
+        rating = 2;
+        success = true;
+        ratingDescription = "very good";
+    } else if (average >= target /2){
+        rating = 1;
+        success = true;
+        ratingDescription = "good";
+    } 
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    };
+}
+
+const args = process.argv.slice(2).map(item => Number(item));
+const target = args.shift();
+const traningHoursOfDay = args;
+
+console.log(calculateExercises(traningHoursOfDay, target));
